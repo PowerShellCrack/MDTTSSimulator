83fb50201586b2c93e5b3a4acf34bf0e6c798c68
PowershellCrack <richard.j.tracy@gmail.com>
2025-02-08 17:58:06 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,22 +1,54 @@
 # MDTTSSimulator
-Test PowerShell scripts with a task sequence environment
 
-## How to use it
+Test MDT task sequence environment items from desktop without launching a deployment
+
+## Install it
+
 1. Install [ADK](https://docs.microsoft.com/en-us/windows-hardware/get-started/adk-install)
 2. Install [MDT](https://www.microsoft.com/en-us/download/details.aspx?id=54259)
 3. Run SetupMDTSimulator.cmd
     - Answer a few questions
-1. Run MDT Simulator desktop shortcut
+4. Run MDT Simulator from desktop shortcut
+5. Once session window is open (Powershell/ISE/Code). You need to run the `C:\MDTSimluator\TSEnv.ps1` script.
 
-## Customizations
+> TIP: Best if ran as elevated Administrator.
 
-Y- ou can add rules to the [customsettings.ini](CustomSettings.ini). 
+## Tested Deployment Shares
 
-## What it does
+- MDT (this is by default)
+- PSD (You must add PSD modules to C:\MDTSimulator\Modules folder)
 
-The _SetupMDTSimulator.cmd_ script automated the MDT setup by:
- - copying required files from MDT distribution folder into a c:\MDTSimulator
- - copying customsettings.ini
- - generating the appropriate TS.xml file base don input
- - generating a run command calling a powershell script to start TS engine
- - and creating a shortcut
+## Simulated Testing
+
+**For MDT:** Add rules to the [customsettings.ini](CustomSettings.mdtsample.ini), then relaunch the simulator
+
+**For PSD:** Within the session window running the simulator, additional rules can be loaded:
+
+```powershell
+#Enable Debugging 
+$PSDDebug = $true
+
+#EXAMPLE: Additional rules can be loaded
+Invoke-PSDRules -FilePath '\\<SERVER>\<MDTSHARE>\Control\CustomSettings.ini' -MappingFile 'C:\MDTSimulator\Modules\PSDGather\ZTIGather.xml'
+
+#EXAMPLE: Launch the new PSDWizard
+Show-PSDWizard -ResourcePath '\\<SERVER>\<MDTSHARE>\Scripts\PSDWizardNew' -Language en-US -Theme Dark -ScriptPath '\\<SERVER>\<MDTSHARE>\Scripts' -Verbose
+```
+
+## What does it do?
+
+The _SetupMDTSimulator.cmd_ script:
+
+- Copies required files from MDT distribution folder into a C:\MDTSimulator
+- Copies CustomSettings.ini from deploymentshare to C:\MDTSimulator
+- Generates the TS.xml file based on input
+- Generates a batch script that will call a powershell script to start TS engine
+- Creates a desktop shortcut
+
+## Known Issues
+
+- Simulator must run in elevated mode; Desktop shortcut is configured to run as Administrator already
+- Running this within Powershell 7 does not work. It won't load the tasksequence bootstrap modules properly.
+- Simulator may fail if set to VSCode (Visual Studio Code) and Powershell 7 is the extension; it must launch with Powershell 5 to work
+- If your deploymentshare CustomSettings.ini is not valid, it may not launch correctly. Overwrite using the sample provided then relaunch
+- Only the session windows that is open will have the TS environment loaded; if closed, it will kill the simulation
--- SetupMDTSimulator.cmd
+++ SetupMDTSimulator.cmd
@@ -5,34 +5,45 @@ ECHO		*							*
 ECHO		*	 Prepare MDT Simulation Directory    		*
 ECHO		*							*
 ECHO		*********************************************************
-ECHO .
 SET MDTSourceFiles=C:\Program Files\Microsoft Deployment Toolkit\Templates\Distribution
 SET INSTALLPATH=%systemdrive%\MDTSimulator
 SET TSSCRIPT=%INSTALLPATH%\OpenEditor.ps1
 
-SET /P DEPSHARE=What is the Deployment share UNC (eg. \\192.168.1.10\Deploymentshare$)? 
+SET /P DEPSHARE=What is the Deployment share UNC [eg. \\192.168.1.10\Deploymentshare$]? 
 
-echo "What is the Editor you'd like to use" 
-echo   A. Powershell
-echo   B. PowerShell ISE
-echo   C. Visual Studio Code
-choice /c ABC /m "What is your selection"
+ECHO What is the Editor you'd like to use: 
+ECHO   A. Powershell
+ECHO   B. PowerShell ISE
+ECHO   C. Visual Studio Code
+choice /c ABC /m "Please select one of the above options"
 IF %ERRORLEVEL% EQU 1 (
-	SET TSNAME=PowerShell
+	SET SIMENV=PowerShell
 	SET EDITORPATH="C:\Windows\system32\WindowsPowerShell\v1.0\PowerShell.exe"
 	SET LAUNCHCMD=
 )
 IF %ERRORLEVEL% EQU 2 (
-	SET TSNAME=ISE
+	SET SIMENV=ISE
 	SET EDITORPATH="C:\Windows\system32\WindowsPowerShell\v1.0\PowerShell_ISE.exe"
 	SET LAUNCHCMD=
 )
 IF %ERRORLEVEL% EQU 3 (
-	SET TSNAME=VSCode
+	SET SIMENV=VSCode
 	SET EDITORPATH="%userprofile%\AppData\Local\Programs\Microsoft VS Code\code.exe"
 	SET POSTARGS=-ArgumentList "%DEPSHARE% %INSTALLPATH%\TSEnv.ps1 --new-window"
 	SET LAUNCHCMD=cmd /c
 )
+
+ECHO Are you simulating MDT or PSD:
+ECHO   A. MDT
+ECHO   B. PSD
+choice /c AB /m "Please select one of the above options"
+IF %ERRORLEVEL% EQU 1 (
+	SET MODE=MDT
+)
+IF %ERRORLEVEL% EQU 2 (
+	SET MODE=PSD
+)
+
 
 ECHO Building %TSSCRIPT% file
 ECHO Start-Process %EDITORPATH% %POSTARGS% -Wait > %TSSCRIPT%
@@ -53,7 +64,6 @@ copy /y "%MDTSourceFiles%\Tools\00000409\tsres.dll" "%INSTALLPATH%\00000409" > N
 copy /y "%MDTSourceFiles%\Tools\x64\CcmCore.dll" "%INSTALLPATH%" > NUL
 copy /y "%MDTSourceFiles%\Tools\x64\ccmgencert.dll" "%INSTALLPATH%" > NUL
 copy /y "%MDTSourceFiles%\Tools\x64\CcmUtilLib.dll" "%INSTALLPATH%" > NUL
-copy /y "%MDTSourceFiles%\Tools\x64\CustomSettings.ini" "%INSTALLPATH%" > NUL
 copy /y "%MDTSourceFiles%\Tools\x64\Smsboot.exe" "%INSTALLPATH%" > NUL
 copy /y "%MDTSourceFiles%\Tools\x64\SmsCore.dll" "%INSTALLPATH%" > NUL
 copy /y "%MDTSourceFiles%\Tools\x64\msvcp120.dll" "%INSTALLPATH%" > NUL
@@ -69,10 +79,13 @@ copy /y "%MDTSourceFiles%\Scripts\ZTIDataAccess.vbs" "%INSTALLPATH%" > NUL
 copy /y "%MDTSourceFiles%\Scripts\ZTIGather.wsf" "%INSTALLPATH%" > NUL
 copy /y "%MDTSourceFiles%\Scripts\ZTIGather.xml" "%INSTALLPATH%" > NUL
 copy /y "%MDTSourceFiles%\Scripts\ZTIUtility.vbs" "%INSTALLPATH%" > NUL
+
+ECHO Copying CustomSettings.ini from %DEPSHARE%
+copy /y "%DEPSHARE%\Control\CustomSettings.ini" "%INSTALLPATH%" > NUL
 timeout 1 > NUL
 
 ECHO Copying TS Files
-copy /y "%~dp0CustomSettings.ini" "%INSTALLPATH%" > NUL
+::copy /y "%~dp0CustomSettings.mdtsample.ini" "%INSTALLPATH%" > NUL
 copy /y "%~dp0TSEnv.ps1" "%INSTALLPATH%" > NUL
 copy /y "%~dp0Start-MDTSimulator.ps1" "%INSTALLPATH%" > NUL
 timeout 1 > NUL
@@ -82,20 +95,20 @@ xcopy /O /X /E /H /K /Y "%MDTSourceFiles%\Tools\Modules" "%windir%\System32\Wind
 timeout 1 > NUL
 
 ECHO Generating TS.XML
-echo ^<^?xml version="1.0"?^> > "%INSTALLPATH%\TS.xml
-echo ^<sequence version="3.00" name="Custom Task Sequence" description="MDT Simulator"^> >> "%INSTALLPATH%\TS.xml
-echo   ^<step type="BDD_InstallApplication" name="Launching %TSNAME% with TS Environment Variables" description="" disable="false" continueOnError="false" runIn="WinPEandFullOS" successCodeList="0 3010"^> >> "%INSTALLPATH%\TS.xml
-echo     ^<defaultVarList^> >> "%INSTALLPATH%\TS.xml
-echo       ^<variable name="ApplicationGUID" property="ApplicationGUID"^>^</variable^> >> "%INSTALLPATH%\TS.xml
-echo       ^<variable name="ApplicationSuccessCodes" property="ApplicationSuccessCodes"^>0 3010^<^/variable^> >> "%INSTALLPATH%\TS.xml
-echo     ^</^defaultVarList^> >> "%INSTALLPATH%\TS.xml
-echo     ^<action^>"C:\Windows\System32\WindowsPowerShell\v1.0\Modules\Microsoft.BDD.TaskSequenceModule\Microsoft.BDD.TaskSequencePSHost40.exe" "%TSSCRIPT%" "C:\MININT\SMSOSD\OSDLOGS"^<^/action^> >> "%INSTALLPATH%\TS.xml
-echo   ^<^/step^> >> "%INSTALLPATH%\TS.xml
-echo ^<^/sequence^> >> "%INSTALLPATH%\TS.xml
+ECHO ^<^?xml version="1.0"?^> > "%INSTALLPATH%\TS.xml
+ECHO ^<sequence version="3.00" name="Custom Task Sequence" description="MDT Simulator"^> >> "%INSTALLPATH%\TS.xml
+ECHO   ^<step type="BDD_InstallApplication" name="Launching %SIMENV% with %MODE% Environment Variables" description="" disable="false" continueOnError="false" runIn="WinPEandFullOS" successCodeList="0 3010"^> >> "%INSTALLPATH%\TS.xml
+ECHO     ^<defaultVarList^> >> "%INSTALLPATH%\TS.xml
+ECHO       ^<variable name="ApplicationGUID" property="ApplicationGUID"^>^</variable^> >> "%INSTALLPATH%\TS.xml
+ECHO       ^<variable name="ApplicationSuccessCodes" property="ApplicationSuccessCodes"^>0 3010^<^/variable^> >> "%INSTALLPATH%\TS.xml
+ECHO     ^</^defaultVarList^> >> "%INSTALLPATH%\TS.xml
+ECHO     ^<action^>"%INSTALLPATH%\Modules\Microsoft.BDD.TaskSequenceModule\Microsoft.BDD.Thunk40.exe" "%INSTALLPATH%\Modules\Microsoft.BDD.TaskSequenceModule\Microsoft.BDD.TaskSequencePSHost40.exe" "%TSSCRIPT%" "C:\MININT\SMSOSD\OSDLOGS"^<^/action^> >> "%INSTALLPATH%\TS.xml
+ECHO   ^<^/step^> >> "%INSTALLPATH%\TS.xml
+ECHO ^<^/sequence^> >> "%INSTALLPATH%\TS.xml
 timeout 1 > NUL
 
 ECHO Generating RunMDTSimulator.cmd
-echo Powershell -ExecutionPolicy Bypass -File "%INSTALLPATH%\Start-MDTSimulator.ps1" -MDTSimulatorPath %INSTALLPATH% -DeploymentShare %DEPSHARE% -Environment %TSNAME% > "%INSTALLPATH%\RunMDTSimulator.cmd
+ECHO Powershell -ExecutionPolicy Bypass -NoProfile -File "%INSTALLPATH%\Start-MDTSimulator.ps1" -Mode %MODE% -MDTSimulatorPath %INSTALLPATH% -DeploymentShare "%DEPSHARE%" -Environment %SIMENV% > "%INSTALLPATH%\RunMDTSimulator.cmd
 timeout 1 > NUL
 
 ECHO Creating Desktop Shortcut
